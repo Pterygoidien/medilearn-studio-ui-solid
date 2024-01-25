ba8e2575afd7549b600f92f141f5f8f97354731a
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-25 04:28:08 +0100

    Component| CourseHeader : (1) destructure props, (2) Change Input field width to adjust to length of content

--- src/routes/courses/[slug]/components/CourseHeader.tsx
+++ src/routes/courses/[slug]/components/CourseHeader.tsx
@@ -1,32 +1,40 @@
-import { Component, JSX, Show, createSignal } from "solid-js";
-
+import { Component, JSX, Show, createEffect, createSignal } from "solid-js";
 
 type TCourseHeaderProps = {
-    courseName: any;
-    setCourseNameFn: (name: string) => void;
-}
+  courseName: any;
+  setCourseNameFn: (name: string) => void;
+};
 
-const CourseHeader: Component<TCourseHeaderProps> = ({ courseName, setCourseNameFn }: TCourseHeaderProps): JSX.Element => {    
+const CourseHeader: Component<TCourseHeaderProps> = ({
+  courseName,
+  setCourseNameFn,
+}: TCourseHeaderProps): JSX.Element => {
     const [editMode, setEditMode] = createSignal<boolean>(false);
 
-
-    return (
-        <header class="py-2 bg-slate-50 px-2">
-            <div class="container flex  items-center mx-auto gap-5">
-   
-                <Show when={editMode()} fallback={
-                    <>
-                        <h2>{courseName()}</h2>
-                        <span onClick={() => setEditMode(s => !s)}>Modifier</span>
-                    </>
-                }>
-                    <input class="text-4xl font-bold leading-5 tracking-tighter" type="text" value={courseName()} onChange={(e)=>setCourseNameFn(e.target.value)} />
-                    <span onClick={() => setEditMode(s => !s)}>Enregistrer</span>
-                </Show>
-               
-            </div>
-        </header>
-    )
-}
+  return (
+    <header class="py-2 bg-slate-50 px-2">
+      <div class="container flex  justify-between items-center mx-auto gap-5">
+        <Show
+          when={editMode()}
+          fallback={
+            <>
+              <h2>{courseName()}</h2>
+              <span onClick={() => setEditMode((s) => !s)}>Modifier</span>
+            </>
+          }
+        >
+          <input
+            class="text-4xl font-bold leading-5 tracking-tighter"
+            style={{ width: `${courseName().length > 10 ? courseName().length : 10}ch` }}
+            type="text"
+            value={courseName()}
+            onInput={(e) => setCourseNameFn(e.target.value)}
+          />
+          <span onClick={() => setEditMode((s) => !s)}>Enregistrer</span>
+        </Show>
+      </div>
+    </header>
+  );
+};
 
 export default CourseHeader;
