b7c68d908a8ca3f481688224c0b7b79349f280a6
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-25 03:36:03 +0100

    Page| Update Course/:slug => Add a Store for section, iterate through section, create mock Course data

--- src/routes/courses/[slug]/index.tsx
+++ src/routes/courses/[slug]/index.tsx
@@ -1,30 +1,94 @@
 import { useParams } from "@solidjs/router";
-import { JSX, createSignal } from "solid-js";
+import { For, JSX, createEffect, createSignal } from "solid-js";
 import CourseHeader from "./components/CourseHeader";
+import { createStore } from "solid-js/store";
+import { TCourseCuriculum } from "~/shared/types/TCurriculum";
+import CourseSection from "./components/CourseSection";
 
 export default function Page(): JSX.Element {
+  const params = useParams();
+  const { slug } = params;
 
-    const params = useParams();
-    const { slug } = params; 
+  const [courseName, setCourseName] = createSignal<string>(slug);
+  const [courseCuriculum, setCourseCuriculum] =
+    createStore<TCourseCuriculum>(mockCourseCuriculum);
 
-    const [courseName, setCourseName] = createSignal<string>(slug);
-    
+  createEffect(() => {
+    //change URL param when courseName change
+    window.history.replaceState(null, "", `/courses/${courseName()}`);
+  });
 
+  const addSection = () => {
+    setCourseCuriculum((prev) => {
+      return {
+        ...prev,
+        sections: [
+          ...prev.sections,
+          {
+            name: "New section",
+            chapters: [],
+          },
+        ],
+      };
+    });
+  };
 
-    return (<main class="container mx-auto flex flex-col gap-3">
-        <CourseHeader name={courseName()} />
-        <section class="flex flex-col gap-3">
-            <h3>Course details</h3>
-            <p>Course description</p>
-        </section>
-        <section>
-            <h3>Course curriculum</h3>
-            Import content / Bulk Edit
-
-            <input type="section" /> Cancel Save
-
-
-            New section
-        </section>
-    </main>)
+  return (
+    <main class="container mx-auto flex flex-col gap-3">
+      <CourseHeader courseName={courseName} setCourseNameFn={setCourseName} />
+      <section class="flex flex-col gap-3">
+        <h3>Course details</h3>
+        <p>Course description</p>
+      </section>
+      <section class="flex flex-col gap-3">
+        <h3>Course curriculum</h3>
+        <div class="bg-gray-100 p-4 rounded-xl">
+          <For each={courseCuriculum.sections}>
+            {(section) => {
+              return <CourseSection section={section} />;
+            }}
+          </For>
+          <button onClick={addSection}>Add section</button>
+        </div>
+        Import content / Bulk Edit New section
+      </section>
+    </main>
+  );
 }
+
+const mockCourseCuriculum: TCourseCuriculum = {
+  name: "Course name",
+
+  sections: [
+    {
+      name: "Section 1",
+      chapters: [
+        {
+          name: "Chapter 1",
+        },
+        {
+          name: "Chapter 2",
+        },
+        {
+          name: "Chapter 3",
+        },
+      ],
+      order: 1,
+    },
+    {
+      name: "Section 2",
+      chapters: [
+        {
+          name: "Chapter 1",
+        },
+        {
+          name: "Chapter 2",
+        },
+        {
+          name: "Chapter 3",
+        },
+      ],
+      order: 2,
+    },
+  ],
+};
