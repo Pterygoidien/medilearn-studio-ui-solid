258de16e29032d1cc1620636b3c02d8f728583b9
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-25 04:24:05 +0100

    Page| Rename Course List index page

--- src/routes/courses/(course).tsx
+++ src/routes/courses/(course).tsx
@@ -3,7 +3,7 @@ import CreateCourseForm from "./new/components/CreateCourseForm";
 
 export default function Page(): JSX.Element {
     return (
-        <main class="container mx-auto flex gap-5">
+        <main class="container mx-auto flex flex-col gap-5">
             See my courses
             <a href="/courses/new">Create a new course</a>
       
