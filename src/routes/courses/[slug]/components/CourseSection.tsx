dabfe19f9b9a904e9a556f43a8d02aba4340a0b5
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-25 04:25:31 +0100

    Component| CourseSection component : Add optional draggable props

--- src/routes/courses/[slug]/components/CourseSection.tsx
+++ src/routes/courses/[slug]/components/CourseSection.tsx
@@ -1,15 +1,29 @@
 import { Component, For, JSX } from "solid-js";
 import { TSection } from "~/shared/types/TSection";
 import CourseChapter from "./CourseChapter";
+import { TDraggable } from "~/shared/types/events/TDraggable";
+
 type Props = {
     section: TSection;
-}
-const CourseSection: Component<Props> = ({ section: { name, chapters }  }:Props): JSX.Element=> {
-    return <article>
-        {name}
-        <For each={chapters}>{
-            (chapter) => <CourseChapter chapter={chapter} />
-        }</For>
+} & TDraggable;
+
+
+const CourseSection: Component<Props> = ({
+  section: { name, chapters },
+  ...props
+}: Props): JSX.Element => {
+
+
+  return (
+    <article
+      class="p-4 bg-slate-200"
+      {...props}
+    >
+      <h4>{name}</h4>
+      <For each={chapters}>
+        {(chapter) => <CourseChapter chapter={chapter} />}
+      </For>
     </article>
-}
+  );
+};
 export default CourseSection;
